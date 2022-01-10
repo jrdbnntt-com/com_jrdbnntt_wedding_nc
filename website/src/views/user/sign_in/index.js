2ce1c5b438626ee0eee528057af1579555e610f6
Jared Bennett <jrdbnntt@gmail.com>
2022-01-10 14:23:14 -0800

    basic page outline with test nav

---
+++ website/src/views/user/sign_in/index.js
@@ -0,0 +1,4 @@
+import "../../../views/_templates/main/index"
+import "./index.scss"
+
+console.log("TODO")
--- website/urls.py
+++ website/urls.py
@@ -1,6 +1,12 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.home, name="home")
+    path('', views.home, name='home'),
+    path('event/', views.event, name='event'),
+    path('user/', include([
+        path('sign_in/', views.user_sign_in, name='user/sign_in'),
+        path('register/', views.user_register, name='user/register'),
+        path('profile/', views.user_profile, name='user/profile'),
+    ]))
 ]
